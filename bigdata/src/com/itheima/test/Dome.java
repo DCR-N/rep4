0d8a277535d71caedfccd046ec01f828338a06a1
Yang <yang@itcast.com>
2020-05-05 22:05:09 +0800

    修改

--- bigdata/src/com/itheima/test/Dome.java
+++ bigdata/src/com/itheima/test/Dome.java
@@ -3,5 +3,6 @@ package com.itheima.test;
 public class Dome {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Wo Shi aa");
     }
 }
